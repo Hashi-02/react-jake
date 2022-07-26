69d2d9ab5c9b1309df2ae326a7de1ebc95c93578
hashi-02 <hassie0209@gmail.com>
2022-07-27 03:24:33 +0900

    add StyledComponets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
 import { StyledJsx } from './components/StyledJsx';
+import { StyledComponets } from './components/StyledComponets';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <InlineStyle />
       <CssModules />
       <StyledJsx />
+      <StyledComponets />
     </div>
   );
 }
