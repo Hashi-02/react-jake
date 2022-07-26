e603a8efa800ceab1faf84c21060df328d56c29c
hashi-02 <hassie0209@gmail.com>
2022-07-27 00:16:01 +0900

    重い実装

--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,5 +1,12 @@
 export const ChildArea = (props) => {
   const { open } = props;
+  //文字入力やボタン押下でレンダリングされる
+  const data = [...Array(2000).keys()];
+  console.log('ChildArea is rendering...(by2000)');
+  data.forEach(() => {
+    console.log('...');
+  });
+
   return (
     <>
       {open ? (
