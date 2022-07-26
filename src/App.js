0341694347c5cad776fc68bdc1e58d5d382bfcf7
hashi-02 <hassie0209@gmail.com>
2022-07-27 02:06:56 +0900

    inlineStyleCSS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { useState, useCallback } from 'react';
 import './App.css';
-import { ChildArea } from './components/ChildArea';
+import { InlineStyle } from './components/InlineStyle';
 
 function App() {
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => {
-    setText(e.target.value);
-  };
-  const onOpen = () => {
-    setOpen(!open);
-  };
-  const onClose = useCallback(() => {
-    setOpen(false);
-  }, []);
-
   return (
     <div className="App">
-      <br />
-      <br />
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onOpen}>表示</button>
-      <ChildArea open={open} onClose={onClose} />
+      <InlineStyle />
     </div>
   );
 }
