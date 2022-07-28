d0ecdbe0584f356b0b6342213f34c8629ae5dc31
hashi-02 <hassie0209@gmail.com>
2022-07-29 05:14:11 +0900

    atomicボタンの表示

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import './App.css';
-// import { InlineStyle } from './components/InlineStyle';
-// import { CssModules } from './components/CssModules';
-// import { StyledJsx } from './components/StyledJsx';
-// import { StyledComponets } from './components/StyledComponets';
-// import { Emotion } from './components/Emotion';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Router } from './router/Router';
+import { PrimaryButton } from './components6/Atoms/button/PrimaryButton';
+import { SecondaryButton } from './components6/Atoms/button/SecondaryButton';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/page1">Page1</Link>
-        <br />
-        <Link to="/page2">Page2</Link>
-        <br />
-      </div>
-      <Router />
-    </BrowserRouter>
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>テスト</SecondaryButton>
+    </div>
   );
 }
 
