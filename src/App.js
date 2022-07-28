50308e16961c01c3f244b0d67a61b8d56be42db3
hashi-02 <hassie0209@gmail.com>
2022-07-28 23:05:23 +0900

    簡単なページ繊維の設定

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
-import { InlineStyle } from './components/InlineStyle';
-import { CssModules } from './components/CssModules';
-import { StyledJsx } from './components/StyledJsx';
-import { StyledComponets } from './components/StyledComponets';
-import { Emotion } from './components/Emotion';
+// import { InlineStyle } from './components/InlineStyle';
+// import { CssModules } from './components/CssModules';
+// import { StyledJsx } from './components/StyledJsx';
+// import { StyledComponets } from './components/StyledComponets';
+// import { Emotion } from './components/Emotion';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { Home } from './components5/Home';
+import { Page1 } from './components5/Page1';
+import { Page2 } from './components5/Page2';
 
 function App() {
   return (
-    <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponets />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
