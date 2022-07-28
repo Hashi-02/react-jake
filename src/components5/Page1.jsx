ff8b8c70b8c59022fadfcef519cc992a9a414986
hashi-02 <hassie0209@gmail.com>
2022-07-28 23:31:04 +0900

    Linkタグを追加

--- src/components5/Page1.jsx
+++ src/components5/Page1.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1</h1>
+      <Link to="/page1/DetailA">DetailA</Link>
+      <br />
+      <Link to="/page1/DetailB">DetailB</Link>
+      <br />
     </div>
   );
 };
