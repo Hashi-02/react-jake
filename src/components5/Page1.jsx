7ae8284ab6f5564766539e6053307454c1e2df6f
hashi-02 <hassie0209@gmail.com>
2022-07-29 03:49:45 +0900

    stateの追加とuseHistoryの使用

--- src/components5/Page1.jsx
+++ src/components5/Page1.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+  const history = useHistory();
+  const onClickDetailA = () => history.push('/page1/DetailA');
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="/page1/DetailA">DetailA</Link>
+      <Link to={{ pathname: '/page1/DetailA', state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/DetailB">DetailB</Link>
       <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
