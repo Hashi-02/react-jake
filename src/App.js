6c9ef8938ef0137861f3d3ad56e6f19723a452cf
hashi-02 <hassie0209@gmail.com>
2022-07-29 07:06:29 +0900

    カードの表示

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import './App.css';
 import { PrimaryButton } from './components6/Atoms/button/PrimaryButton';
 import { SecondaryButton } from './components6/Atoms/button/SecondaryButton';
 import { SearchInput } from './components6/molecules/SearchInput';
+import { UserCard } from './components6/organisms/user/UserCard';
 
 function App() {
+  const user = {
+    name: 'はし',
+    image: 'https://source.unsplash.com/p6yH8VmGqxo',
+    email: 'exsample@email.com',
+    phone: 'XXXX-XXXX-XXXX',
+    company: {
+      name: 'テスト会社',
+    },
+    website: 'https://google.com',
+  };
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>テスト</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
