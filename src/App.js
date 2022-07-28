a72f51afbc2821b673a3e80833aa0ecdbe231062
hashi-02 <hassie0209@gmail.com>
2022-07-29 05:50:59 +0900

    Inputタグ表示

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { PrimaryButton } from './components6/Atoms/button/PrimaryButton';
 import { SecondaryButton } from './components6/Atoms/button/SecondaryButton';
+import { SearchInput } from './components6/molecules/SearchInput';
 
 function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>テスト</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
