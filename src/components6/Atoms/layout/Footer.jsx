47c0203f242e7b6ced68d761d7a2a439ba082ca6
hashi-02 <hassie0209@gmail.com>
2022-07-30 23:45:34 +0900

    不要なコードを削除

--- src/components6/Atoms/layout/Footer.jsx
+++ src/components6/Atoms/layout/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return <SFooter>&copy; 2022 react-udemy</SFooter>;
