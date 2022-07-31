9c99fa05ccb7e156bebe905e8cd8179a7e1f3869
hashi-02 <hassie0209@gmail.com>
2022-08-01 07:01:07 +0900

    Top.jsxの作成

---
+++ src/components6/pages/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+export const Top = () => {
+  return (
+    <SContainer>
+      <h2>Topページ</h2>
+    </SContainer>
+  );
+};
+const SContainer = styled.div`
+  text-align: center;
+`;
