fc1b86d853fedaa136e12659e6d1f5556d1b7416
hashi-02 <hassie0209@gmail.com>
2022-08-01 07:00:48 +0900

    Routerコンポーネントに移行

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { PrimaryButton } from './components6/Atoms/button/PrimaryButton';
-import { SecondaryButton } from './components6/Atoms/button/SecondaryButton';
-import { SearchInput } from './components6/molecules/SearchInput';
-import { UserCard } from './components6/organisms/user/UserCard';
-import { DefaultLayouts } from './components6/templates/DefaultLayouts';
+import { Router } from './components6/router/Router';
 
 function App() {
-  const user = {
-    name: 'はし',
-    image: 'https://source.unsplash.com/p6yH8VmGqxo',
-    email: 'exsample@email.com',
-    phone: 'XXXX-XXXX-XXXX',
-    company: {
-      name: 'テスト会社',
-    },
-    website: 'https://google.com',
-  };
-  return (
-    <BrowserRouter>
-      <DefaultLayouts>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>テスト</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayouts>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
