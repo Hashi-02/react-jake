b3071b81da270fb1651a526cfc67a38434dedc8a
hashi-02 <hassie0209@gmail.com>
2022-08-01 08:43:44 +0900

    SecondaryButtonの追加

--- src/components6/pages/Top.jsx
+++ src/components6/pages/Top.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SecondaryButton } from '../Atoms/button/SecondaryButton';
+import { useHistory } from 'react-router-dom';
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () =>
+    history.push({ pathname: '/user', state: { isAdmin: true } });
+  const onClickGeneral = () =>
+    history.push({ pathname: '/user', state: { isAdmin: false } });
   return (
     <SContainer>
       <h2>Topページ</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザ</SecondaryButton>
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザ</SecondaryButton>
     </SContainer>
   );
 };
