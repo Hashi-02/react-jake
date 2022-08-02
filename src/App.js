0dcb8bf3cd7530ea7186ac602cecfc5d797d145f
hashi-02 <hassie0209@gmail.com>
2022-08-02 15:28:05 +0900

    axiosとjsonplaceholderのテスト

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+import axios from 'axios';
 import './App.css';
-import { Router } from './components6/router/Router';
-import { UserProvider } from './components6/providers/UserProvider';
-import { RecoilRoot } from 'recoil';
+// import { Router } from './components6/router/Router';
+// import { UserProvider } from './components6/providers/UserProvider';
+// import { RecoilRoot } from 'recoil';
 
 function App() {
+  const onClickUsers = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUsers1 = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <RecoilRoot>
-      <UserProvider>
-        <Router />
-      </UserProvider>
-    </RecoilRoot>
+    // <RecoilRoot>
+    //   <UserProvider>
+    //     <Router />
+    //   </UserProvider>
+    // </RecoilRoot>
+    <>
+      <button onClick={onClickUsers}>user</button>
+      <button onClick={onClickUsers1}>user1</button>
+    </>
   );
 }
 
